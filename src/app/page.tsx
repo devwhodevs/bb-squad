ac22d9b30c31a0a35ed91480a52a1bca60e03adf
Oleksandr Ostrovskyi <devwhodevs@gmail.com>
2024-08-11 15:08:57 +0300

    Improve UI and update content
    Refactored layout and main content structure for better readability and user experience. Added styles to make the page fully responsive and included missing "dark" class for dark mode support. Updated greeting text and Discord link to support localization.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,37 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Ласкаво Просимо у сім'ю!
+    <main className="flex min-h-screen flex-col items-center justify-between w-full p-4">
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <p
+          className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Вітаємо сайті паблік клану BB
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+      </div>
+
+      <div
+        className="relative flex place-items-center">
+        <div className="flex items-center justify-between gap-16 z-10">
+          <div className="flex items-center gap-4">
+            <img src="https://cdn.discordapp.com/embed/avatars/0.png?size=128" alt="Discord"
+                 className="h-14 w-14 rounded-xl" draggable="false"/>
+            <div>
+              <h1 className="cursor-pointer font-normal text-[#060607] dark:text-white">Discord</h1>
+            </div>
+          </div>
+          <a target="_blank" rel="noopener noreferrer" className="cursor-pointer focus-visible:ring-ring ring-offset-background inline-flex h-10 items-center justify-center rounded-md bg-[#248046] px-4 py-2 text-sm font-medium text-[#e9ffec] transition-colors hover:bg-[#1a6334] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" href="https://discord.gg/ajxW3jEjjD">
+            Приєднатись
+          </a>
         </div>
       </div>
 
-        <div
-            className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-            <div className="flex items-center justify-between gap-16">
-                <div className="flex items-center gap-4">
-                    <img src="https://cdn.discordapp.com/embed/avatars/0.png?size=128" alt="Discord"
-                         className="h-14 w-14 rounded-xl" draggable="false"/>
-                    <div>
-                        <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/ajxW3jEjjD"><h1
-                            className="cursor-pointer font-normal text-[#060607] hover:underline dark:text-white">Discord</h1>
-                        </a>
-                    </div>
-                </div>
-                <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/ajxW3jEjjD">
-                    <button
-                        className="focus-visible:ring-ring ring-offset-background inline-flex h-10 items-center justify-center rounded-md bg-[#248046] px-4 py-2 text-sm font-medium text-[#e9ffec] transition-colors hover:bg-[#1a6334] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">Join
-                    </button>
-                </a>
-            </div>
-        </div>
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <p
+          className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Ми не ті самі токсичні клани, які ви звикли бачити. <br/>
+          Ми віримо в дружбу, взаємопідтримку та взаємоповагу. <br/>
+          Ласкаво Просимо у сім'ю!
+        </p>
+      </div>
     </main>
   );
 }
